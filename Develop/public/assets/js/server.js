7a125fc1e874358db9b640a8593d2123c5cd23d2
tripledawg <iamlesliecrouch>
2022-01-17 19:37:46 -0500

    moved over starter code and created server.js file

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,21 @@
+//const express = require('express');
+// const path = require('path');
+
+// const app = express();
+// const PORT = 3001;
+
+// app.use(express.static('public')); //middleware
+
+// app.get('/', (req, res) => res.send('Navigate to /send or /routes'));  //if you do a get('/endpoint', (req, res)) then you are assigning an endpoint
+
+// app.get('/send', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/someFile.html'))
+// );
+
+// app.get('/routes', (req, res) =>
+//   res.sendFile(path.join(__dirname, 'public/routes.html'))
+// );
+
+// app.listen(PORT, () =>
+//   console.log(`Example app listening at http://localhost:${PORT}`)
+// ); 
