e795f5c67eb09574b20e2054037bfc32931f0578
tripledawg <iamlesliecrouch>
2022-01-17 19:44:40 -0500

    server.js code started

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,7 +1,7 @@
-//const express = require('express');
-// const path = require('path');
+const express = require('express');
+const path = require('path');
 
-// const app = express();
+const app = express();
 // const PORT = 3001;
 
 // app.use(express.static('public')); //middleware
@@ -19,3 +19,6 @@
 // app.listen(PORT, () =>
 //   console.log(`Example app listening at http://localhost:${PORT}`)
 // ); 
+
+// app.get('/api/notes', (req, res)) => res.send('')
+
