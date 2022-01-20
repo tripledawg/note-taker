98e8ba5cf5716ebf2aed19359a9c5af2941b8386
tripledawg <iamlesliecrouch>
2022-01-19 22:32:19 -0500

    working on post request

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const db = require('./db/db.json');
 // const { notStrictEqual } = require('assert');
 
 const app = express();
 const PORT = process.env.prt || 3001;
 
 app.use(express.static('public')); //middleware
+app.use(express.json());
 
 app.get('/api/notes', (req, res) => {  //endpoint 
     res.sendFile(path.join(__dirname, 'db/db.json'));
@@ -16,15 +18,27 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
-//if you do a get('/endpoint', (req, res)) then you are assigning an endpoint
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html')) //fallback route in case route not defined
+);
 
-// app.get('/send', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/someFile.html'))
-// );
-
-// app.get('/routes', (req, res) =>
-//   res.sendFile(path.join(__dirname, 'public/routes.html'))
-// );
+app.post('/api/notes', (req, res) => {
+    if (req.body) {
+        let note = req.body;
+        console.log(req.body);
+        let dataBase = db;
+        dataBase.push(note);
+        fs.writeFile('/db/db.json', dataBase.toString(), (err, data) => {
+            if (err) throw err;
+            console.log(data);
+        });
+        res.status(200).json(`Note added!`);
+        return;
+    }
+    else {
+        res.status(400).json('Note not found in request');
+    }
+});
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
