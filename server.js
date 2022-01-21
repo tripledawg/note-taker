d2253d3b8161f48dea2d9b0be7da5481c820691f
tripledawg <iamlesliecrouch>
2022-01-21 11:10:08 -0500

    used JSON.stringify to print out db as a string for the file and added uuid v 4

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const db = require('./db/db.json');
-// const { notStrictEqual } = require('assert');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.prt || 3001;
@@ -26,19 +26,45 @@ app.post('/api/notes', (req, res) => {
     if (req.body) {
         let note = req.body;
         console.log(req.body);
-        let dataBase = db;
-        dataBase.push(note);
-        fs.writeFile('/db/db.json', dataBase.toString(), (err, data) => {
+        note.uuid = uuidv4();
+        db.push(note);
+        fs.writeFile('./db/db.json', JSON.stringify(db), (err, data) => {
             if (err) throw err;
-            console.log(data);
+            console.log('Data is : ' + data);
         });
-        res.status(200).json(`Note added!`);
+        res.status(200).json(note);
         return;
     }
     else {
         res.status(400).json('Note not found in request');
     }
 });
+
+// ///
+// const { title, text } = req.body;
+
+// if (title && text) {
+//     // Variable for the object we will save
+//     const newNote = {
+//       title,
+//       text,
+//       upvotes: Math.floor(Math.random() * 100),
+//       review_id: uuid(),
+//     };
+
+// // Convert the data to a string so we can save it
+// const noteString = JSON.stringify(newNote);
+
+// // Write the string to a file
+// fs.writeFile(`./db/${newNote.title}.json`, noteString, (err) =>
+//   err
+//     ? console.error(err)
+//     : console.log(
+//         `Review for ${newNote.title} has been written to JSON file`
+//       )
+// );
+// ////
+
 
 app.listen(PORT, () =>
     console.log(`Example app listening at http://localhost:${PORT}`)
